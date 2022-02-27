707ff603dba357308f1833345f13e18edc0b15b9
Clyde Broderick <clyderick3@gmail.com>
2022-02-27 01:38:34 -0500

    Added a scanner.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
+Scanner scanner = new Scanner(System.in);
+
 
     public static void main(String[] args){
-        
+        for
+        System.out.println("Guess a number.");
     }
 }
