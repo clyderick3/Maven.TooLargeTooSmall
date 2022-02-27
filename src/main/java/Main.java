bfb09fce8030c43e0f04f6d8bf55a46ac25090ec
Clyde Broderick <clyderick3@gmail.com>
2022-02-27 01:50:06 -0500

    Added many integers.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,25 @@
+import java.util.Random;
 import java.util.Scanner;
 
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
+    //added scanner for system to scan number
 Scanner scanner = new Scanner(System.in);
-
+//added random utility which might be the same
+// exact thing as scanner but scans for a random number
+Random r = new Random();
+int low = 1;
+int high = 50;
+int result = r.nextInt();
+//Counting the amount of times the guesser has guessed
+int timesTried = 0;
+//Max amount of times the guesser can guess
+int maxTries = 7;
 
     public static void main(String[] args){
-        for
+
         System.out.println("Guess a number.");
     }
 }
